641ecfd8db9a258edcde11d2bba3c9e30b08cbb9
Armaan124-P <dasarmaan124@gmail.com>
2026-04-03 11:08:46 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+def query (user_query): 
+
+    api_key="AIzaSyA7jD7LpF13hQ2MbLCHXglWboqsvbYU8RQ"
+
+    from google import  genai
+
+    my_ai = genai.Client(api_key = api_key)
+
+    responce = my_ai.models.generate_content(
+        model = "gemini-3-flash-preview",
+        contents = user_query
+    )
+
+    return responce.text
+
+if "message" not in st.session_state:
+    st.session_state.message = []
+
+
+
+st.title("MY PERSONAL AI CHATBOT")
+
+for i in st.session_state.message:
+    with st.chat_message(i["role"]):
+        st.markdown(i["msg"])
+
+user_input = st.chat_input("enter you question")
+
+if user_input :
+    st.session_state.message.append({
+        "role": "user",
+        "msg" : user_input
+
+    })
+    with st.chat_message("user"):
+        st.markdown(user_input)
+
+    with st.chat_message("ai"):
+        with st.spinner("wait"):
+            result = query (user_input)    
+            st.markdown(result)
+    st.session_state.message.append({
+        "role": "ai",
+        "msg" : result
+
+    })         
